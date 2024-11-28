80b2cf5af1431ac0a37359a121389e003c6b8318
Papa Kofi Boahen <papakofiboahen@gmail.com>
2024-11-28 21:20:53 +0000

    added api-docs

--- index.js
+++ index.js
@@ -2,7 +2,32 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const hoganMiddleware = require('hogan-middleware');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Simple Express Application',
+      version: '1.0.0',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Development server',
+      },
+      {
+        url: 'https://simple-express-proj-b3a02135f433.herokuapp.com/',
+        description: 'Production server',
+      }
+    ]
+  },
+  apis: ['./index.js'], // files containing annotations as above
+};
+
+const openapiSpecification = swaggerJSDoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
 // set up the middleware
 app.set('views', path.join(__dirname, 'views')); // tell express which directory your views are in
@@ -19,7 +44,15 @@ const register = require('./routes/register');
 app.use('/', routes.router);
 app.use('/register', register);
 
-// root route
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     description: This is the base url/route
+ *     responses:
+ *       200:
+ *         description: Returns a welcome page.
+ */
 app.get('/', (req, res) => {
   res.send('A simple express CRUD application hosted on Heroku with a PostgreSQL database \n Author: Papa Kofi Boahen');
 });
